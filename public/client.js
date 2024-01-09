a134222a564127608e6057713660cbd9cbf38e4e
AungThuHeain <hmm.aungthuheain@gmail.com>
2024-01-09 23:31:49 +0630

    tenant and live private

--- public/client.js
+++ public/client.js
@@ -7,20 +7,20 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
-//connect to socket server
-// const socket = io("https://socket-ie16.onrender.com", {
-//   transports: ["websocket"],
-// });
-
 //get dom
 const form = document.querySelector("form");
 const userName = document.querySelector("#name");
 const msg = document.querySelector("#msg");
 const chat = document.querySelector(".chat-text");
 const typing = document.querySelector(".typing");
-
+const userList = document.querySelector(".user-list");
+const allUser = [];
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  socket.connect();
+  socket.auth = { name: userName.value };
+
   const data = {
     name: userName.value,
     msg: msg.value,
@@ -45,4 +45,38 @@ socket.on("typing", (name) => {
   setTimeout(() => {
     typing.innerHTML = "";
   }, 3000);
+});
+
+//error handle
+socket.on("connect_error", (err) => {
+  console.log(err.message); // prints the message associated with the error
+});
+
+//show user list
+socket.on("users", (users) => {
+  users.forEach((user) => {
+    userList.innerHTML += `<button id='${user.userId}' class='specific-user'>${user.userName}</button>`;
+    allUser.push(user);
+  });
+});
+
+//emit private message
+userList.addEventListener("click", function (e) {
+  if (e.target.classList.contains("specific-user")) {
+    const data = {
+      name: userName.value,
+      msg: msg.value,
+    };
+    socket.emit("private message", {
+      data: data,
+      to: e.target.id,
+    });
+  }
+});
+
+//accept private message
+socket.on("private message", ({ data, from }) => {
+  (userName.value = ""), (msg.value = "");
+  chat.innerHTML += `Private message => ${data.name} : ${data.msg}</p>`;
+  typing.innerHTML = "";
 });
--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const socket = require("socket.io");
 var path = require("path");
+const { log } = require("console");
 
 /**create express app */
 const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 /**create express server */
-const server = app.listen("3000", () => {
-  console.log("server run on port 3000");
+const server = app.listen("4000", () => {
+  console.log("server run on port 4000");
 });
 
 app.get("/:dynamic", (res, req) => {
@@ -22,21 +23,51 @@ const io = socket(server, {
   },
 });
 
-const dynamicNsp = io.of(/^\/dynamic-\d+$/);
+const tenant = io.of(/^\/dynamic-\d+$/);
 
-dynamicNsp.on("connection", (socket) => {
-  console.log("User connected on dynamic namespace " + socket.nsp.name);
+//middleware
+tenant.use((socket, next) => {
+  const userName = socket.handshake.auth.name;
+
+  if (!userName) {
+    return next(new Error("Invalid username"));
+  }
+  socket.username = userName;
+  next();
+});
+
+tenant.on("connection", async (socket) => {
+  console.log(
+    `User '${socket.username}' connected on organization '${socket.nsp.name}' server`
+  );
 
   const nameSpace = socket.nsp;
 
-  socket.on("message", function (data) {
-    console.log("A message was received from a client: ", data);
+  const users = [];
+  for (let [id, socket] of nameSpace.sockets) {
+    users.push({
+      userId: id,
+      userName: socket.username,
+    });
+  }
 
+  socket.emit("users", users);
+
+  socket.on("message", function (data) {
+    // console.log("A message was received from a client: ", data);
     nameSpace.emit("chat", data);
   });
 
+  //handle typing
   socket.on("typing", (name) => {
-    console.log("tyoing");
     socket.broadcast.emit("typing", name);
+  });
+
+  //handle private message
+  socket.on("private message", ({ data, to }) => {
+    socket.to(to).emit("private message", {
+      data: data,
+      from: socket.id,
+    });
   });
 });
