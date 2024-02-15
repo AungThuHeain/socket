67f0ffa6f3121efe4d462c586de526647f047ffa
AungThuHeain <hmm.aungthuheain@gmail.com>
2024-02-15 12:42:50 +0630

    zz

--- public/client/ui.js
+++ public/client/ui.js
@@ -28,6 +28,10 @@ document.addEventListener("DOMContentLoaded", function () {
     </div>
     <div class="flex mb-10">
     <input type="text" id="msg" required placeholder="Type message.." name="msg" />
+    <div class="">
+    <input type="file" onchange="upload(this.files,this.id)"  id="file" class="inputfile"/>
+    <label for="file" style="margin-top:20px;"><svg width="22" height="22" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M13 18.999a.974.974 0 0 0 .196.563l-1.79 1.81a5.5 5.5 0 1 1-7.778-7.78L15.185 2.159a4 4 0 0 1 5.63 5.685L10.259 18.276a2.5 2.5 0 0 1-3.526-3.545l8-7.999.706.707-8 8a1.5 1.5 0 0 0 2.116 2.126L20.111 7.132a3 3 0 1 0-4.223-4.263L4.332 14.304a4.5 4.5 0 1 0 6.364 6.364L13 18.338zM19 14h-1v4h-4v.999h4V23h1v-4.001h4V18h-4z"/><path fill="none" d="M0 0h24v24H0z"/></svg></label>
+    </div>
     <button type="submit" class="send-btn"><svg alt="Send" class="p-1" xmlns="http://www.w3.org/2000/svg" height="22" width="22">
     <path d="M3 20V4L22 12ZM5 17 16.85 12 5 7V10.5L11 12L5 13.5ZM5 17V12V7V10.5V13.5Z" ></path></svg></button>
     </div>
